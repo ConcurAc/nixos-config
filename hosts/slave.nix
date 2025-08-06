1596e201a73a0e0a5d8e8c5e369de81db9858b63
Connor Davis <concurac@proton.me>
2025-08-06 22:48:40 +1000

    Added new host implementation with disko partitioning

---
+++ hosts/slave.nix
@@ -0,0 +1,112 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+{
+  imports =
+    with inputs;
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+      disko.nixosModules.disko
+    ]
+    ++ (with nixos-hardware.nixosModules; [
+      common-cpu-intel
+      common-pc
+      common-pc-ssd
+    ]);
+
+  boot = {
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ehci_pci"
+      "ahci"
+      "nvme"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+      "sr_mod"
+    ];
+    kernelModules = [ "kvm-intel" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [ "quiet" ];
+
+    loader = {
+      grub = {
+        efiSupport = true;
+        device = "nodev";
+      };
+      efi.canTouchEfiVariables = true;
+    };
+
+    tmp.useZram = true;
+  };
+
+  disko.devices = {
+    disk = {
+      internal = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              name = "ESP";
+              type = "EF00";
+              start = "1M";
+              end = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/@" = {
+                    mountpoint = "/";
+                  };
+                  "/@home" = {
+                    mountpoint = "/home";
+                  };
+                  "/@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  zramSwap.enable = true;
+
+  networking = {
+    hostName = "slave";
+    networkmanager.enable = true;
+  };
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
