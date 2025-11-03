592c2f25e851cb310d016af01debb672a616327a
Connor Davis <concurac@proton.me>
2025-11-03 20:21:38 +1100

    fix: changed connor shell. nushell cannot be used to login to zed remote session

--- users/connor/default.nix
+++ users/connor/default.nix
@@ -39,7 +39,7 @@ in
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2mcsUw0CZ5ktg3c6FG91OGfO8mGCKImZ1aLOmdwl5a"
     ];
-    shell = pkgs.nushell;
+    shell = pkgs.fish;
     linger = true;
     packages = with pkgs; [
       home-manager
