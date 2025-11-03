3e23b1f4b36030e508535f32a14df9d877cb36f2
Connor Davis <concurac@proton.me>
2025-11-03 18:09:17 +1100

    feature: add connor container config with various services

---
+++ users/connor/container.nix
@@ -0,0 +1,92 @@
+{ config, pkgs, ... }:
+let
+  cfg = config.users.users.connor;
+  cfgPhotoPrism = config.services.photoprism;
+  cfgTrilium = config.services.trilium-server;
+  secrets = config.sops.secrets;
+in
+{
+  sops = {
+    secrets = {
+      syncthing-passwd = {
+        sopsFile = ./secrets.yaml;
+        owner = cfg.name;
+        group = cfg.name;
+      };
+      photoprism-passwd = {
+        sopsFile = ./secrets.yaml;
+        mode = "0444";
+      };
+      "livebook.env" = {
+        sopsFile = ./livebook.env;
+        format = "dotenv";
+        key = "";
+        owner = cfg.name;
+        group = cfg.group;
+      };
+    };
+  };
+
+  networking = {
+    firewall.allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+
+  console.enable = true;
+  services = {
+    openssh.enable = true;
+    syncthing = {
+      enable = true;
+      openDefaultPorts = true;
+      guiPasswordFile = secrets.syncthing-passwd.path;
+    };
+    photoprism = {
+      enable = true;
+      originalsPath = "/mnt/gallery";
+      settings = {
+        PHOTOPRISM_ADMIN_USER = "connor";
+        PHOTOPRISM_ADMIN_PASSWORD_FILE = secrets.photoprism-passwd.path;
+        PHOTOPRISM_DATABASE_PASSWORD = secrets.photoprism-passwd.path;
+      };
+    };
+    trilium-server = {
+      enable = true;
+    };
+    livebook = {
+      enableUserService = true;
+      environment = {
+        LIVEBOOK_PORT = 8080;
+      };
+      environmentFile = secrets."livebook.env".path;
+    };
+    nginx = {
+      enable = true;
+      virtualHosts = {
+        "photoprism.connor.me" = {
+          locations."/" = {
+            proxyPass = "http://${cfgPhotoPrism.address}:${toString cfgPhotoPrism.port}";
+            proxyWebsockets = true;
+          };
+        };
+        "syncthing.connor.me" = {
+          locations."/" = {
+            proxyPass = "http://${config.services.syncthing.guiAddress}";
+          };
+        };
+        "trilium.connor.me" = {
+          locations."/" = {
+            proxyPass = "http://${cfgTrilium.host}:${toString cfgTrilium.port}";
+            proxyWebsockets = true;
+          };
+        };
+        "livebook.connor.me" = {
+          locations."/" = {
+            proxyPass = "http://localhost:${toString config.services.livebook.environment.LIVEBOOK_PORT}";
+          };
+        };
+      };
+    };
+  };
+}
--- users/connor/default.nix
+++ users/connor/default.nix
@@ -1,23 +1,28 @@
 {
   inputs,
   config,
+  lib,
   pkgs,
   ...
 }:
 let
   cfg = config.users.users.connor;
+  cfgContainer = config.container-services.users.connor;
+  userKeyFile = "${cfg.home}/.config/sops/age/keys.txt";
 in
 {
   imports = [
-    ../../modules/container-services
+    ../../modules/user-containers.nix
   ]
   ++ (with inputs; [
     sops-nix.nixosModules.sops
   ]);
 
-  sops.secrets.connor-passwd = {
-    sopsFile = ./secrets.yaml;
-    neededForUsers = true;
+  sops.secrets = {
+    connor-passwd = {
+      sopsFile = ./secrets.yaml;
+      neededForUsers = true;
+    };
   };
 
   users.users.connor = {
@@ -32,7 +37,7 @@ in
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2mcsUw0CZ5ktg3c6FG91OGfO8mGCKImZ1aLOmdwl5a"
     ];
-    shell = pkgs.nushell;
+    shell = pkgs.fish;
     linger = true;
     packages = with pkgs; [
       home-manager
@@ -40,19 +45,24 @@ in
     ];
   };
 
-  container-services.users.connor = {
+  user-containers.users.connor = {
     container = {
       enable = true;
       withMacvlan = true;
-      config = {
-        imports = [ ../../modules/terminal.nix ];
-        console.enable = true;
-        services = {
-          syncthing = {
-            enable = true;
-            openDefaultPorts = true;
-          };
+      bindMounts = {
+        ${userKeyFile}.hostPath = config.sops.age.keyFile;
+        "/mnt/gallery" = {
+          hostPath = "$/mnt/users/${cfg.name}/gallery";
+          isReadOnly = false;
         };
+      };
+      config = {
+        imports = [
+          inputs.sops-nix.nixosModules.sops
+          ../../modules/terminal.nix
+          ./container.nix
+        ];
+        sops.age.keyFile = userKeyFile;
       };
     };
   };
@@ -63,6 +73,32 @@ in
         user="${cfg.name}"
         mountpoint="${cfg.home}/Media"
         path="gocryptfs#${cfg.home}/.crypt/media"
+        fstype="fuse"
+      />
+    ''
+  ]
+  ++ lib.mkIf (config.container-services.enable && cfgContainer.enable) [
+    ''
+      <volume
+        user="${cfg.name}"
+        mountpoint="/mnt/users/${cfg.name}/media"
+        path="gocryptfs#/mnt/users/${cfg.name}/.crypt/@media"
+        fstype="fuse"
+      />
+    ''
+    ''
+      <volume
+        user="${cfg.name}"
+        mountpoint="/mnt/users/${cfg.name}/music"
+        path="gocryptfs#/mnt/users/${cfg.name}/.crypt/@music"
+        fstype="fuse"
+      />
+    ''
+    ''
+      <volume
+        user="${cfg.name}"
+        mountpoint="/mnt/users/${cfg.name}/gallery"
+        path="gocryptfs#/mnt/users/${cfg.name}/.crypt/@gallery"
         fstype="fuse"
       />
     ''
