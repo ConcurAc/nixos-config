074c00ee9d043553b939d747178011141db9e6fa
Connor Davis <connor@scequ.com>
2026-06-01 23:59:32 +1000

    update resources module

--- resources/default.nix
+++ resources/default.nix
@@ -1,4 +1,10 @@
 {
-  ca = import ./ca;
-}
+  ca = {
+    root = ./ca/root_ca.crt;
+    intermediate = ./ca/intermediate_ca.crt;
+  };
 
+  palette = {
+    hephae-soft = ./palette/hephae-soft.yaml;
+  };
+}
