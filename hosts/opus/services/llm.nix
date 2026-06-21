01c553bdaef6cd63cc2ce3341840bd93d529c46d
Connor Davis <connor@scequ.com>
2026-06-21 16:03:36 +1000

    hosts/opus: idm integration with services

--- hosts/opus/services/llm.nix
+++ hosts/opus/services/llm.nix
@@ -1,13 +1,52 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
+  secrets = config.sops.secrets;
   hosts = {
     localhost = "localhost:${toString config.services.open-webui.port}";
   };
 in
 {
+  sops.secrets = {
+    "open-webui.env" = {
+      sopsFile = secrets/open-webui.env;
+      format = "dotenv";
+      key = "";
+    };
+    "kanidm/oauth2/open-webui" = {
+      sopsFile = ./secrets/oauth2.yaml;
+      key = "oauth2/open-webui";
+      owner = "kanidm";
+      group = "kanidm";
+    };
+  };
+
   services.open-webui = {
     enable = true;
     port = 8060;
+    environment = {
+      WEBUI_URL = "https://llm.home.arpa";
+
+      OPENID_PROVIDER_URL = "https://id.home.arpa/oauth2/openid/open-webui/.well-known/openid-configuration";
+      OPENID_REDIRECT_URI = "https://llm.home.arpa/oauth/oidc/callback";
+
+      OAUTH_PROVIDER_NAME = "Kanidm";
+      OAUTH_CLIENT_ID = "open-webui";
+
+      OAUTH_SCOPES = "openid email profile groups";
+
+      OAUTH_ALLOWED_ROLES = "open-webui-user,open-webui-admin";
+      OAUTH_ADMIN_ROLES = "open-webui-admin";
+      OAUTH_ROLES_CLAIM = "open-webui-roles";
+
+      OAUTH_CODE_CHALLENGE_METHOD = "S256";
+      OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "true";
+
+      ENABLE_OAUTH_SIGNUP = "true";
+      ENABLE_OAUTH_ROLE_MANAGEMENT = "true";
+
+      SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+    };
+    environmentFile = secrets."open-webui.env".path;
   };
 
   services.nginx.upstreams.open-webui.servers.${hosts.localhost} = { };
@@ -21,5 +60,24 @@ in
     extraConfig = ''
       proxy_buffering off;
     '';
+  };
+
+  services.kanidm.provision.systems.oauth2.open-webui = {
+    displayName = "AI Chat";
+    originUrl = "https://llm.home.arpa/oauth/oidc/callback";
+    originLanding = "https://llm.home.arpa";
+    basicSecretFile = secrets."kanidm/oauth2/open-webui".path;
+    preferShortUsername = true;
+    scopeMaps."users" = [
+      "openid"
+      "email"
+      "profile"
+      "groups"
+    ];
+    claimMaps.open-webui-roles.valuesByGroup = {
+      "admins" = [ "open-webui-admin" ];
+      "users" = [ "open-webui-user" ];
+    };
+    imageFile = "${pkgs.open-webui}/lib/python3.13/site-packages/open_webui/static/favicon.svg";
   };
 }
--- hosts/opus/services/passwords.nix
+++ hosts/opus/services/passwords.nix
@@ -1,5 +1,24 @@
 { config, lib, ... }:
+let
+  inherit (config.sops) secrets;
+in
 {
+  sops.secrets = {
+    "vaultwarden.env" = {
+      sopsFile = secrets/vaultwarden.env;
+      format = "dotenv";
+      key = "";
+      owner = "vaultwarden";
+      group = "vaultwarden";
+    };
+    "kanidm/oauth2/vaultwarden" = {
+      sopsFile = ./secrets/oauth2.yaml;
+      key = "oauth2/vaultwarden";
+      owner = "kanidm";
+      group = "kanidm";
+    };
+  };
+
   services.vaultwarden = {
     enable = true;
     configureNginx = true;
@@ -8,12 +27,34 @@
       ENABLE_WEBSOCKET = true;
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
+      SSO_ENABLED = true;
+      SSO_CLIENT_ID = "vaultwarden";
+      SSO_AUTHORITY = "https://id.home.arpa/oauth2/openid/vaultwarden";
     };
+    environmentFile = secrets."vaultwarden.env".path;
   };
 
   services.nginx.virtualHosts.${config.services.vaultwarden.domain} = {
     forceSSL = lib.mkForce false; # for acme http-01
     addSSL = true;
     enableACME = true;
+  };
+
+  services.kanidm.provision.systems.oauth2.vaultwarden = {
+    displayName = "Passwords";
+    public = true;
+    originUrl = "https://passwords.home.arpa/identity/connect/oidc-signin";
+    originLanding = "https://passwords.home.arpa";
+    basicSecretFile = secrets."kanidm/oauth2/vaultwarden".path;
+    preferShortUsername = true;
+    scopeMaps."users" = [
+      "openid"
+      "email"
+      "profile"
+    ];
+    imageFile = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/dani-garcia/vaultwarden/main/resources/vaultwarden-logo-auto.svg";
+      sha256 = "sha256:15dmz8iivyq3824ip56kvznfrh2lg3mj5cvxnzyf17pa584sic76";
+    };
   };
 }
